13b3ba77d421b1982c36587dc3c9b1be79e7cdab
damiansergi <72512614+damiansergi@users.noreply.github.com>
2020-11-05 19:46:45 -0300

    Add files via upload

---
+++ damian/hardware.h
@@ -0,0 +1,14 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+
+#include "stdio.h"
+#include "emulador.h"
+
+void inicializarHard (void);
+void actualizarHard (void);
+void destruirHard (void);
+char* identificador (int);
+
+#endif	// HARDWARE_H
+
